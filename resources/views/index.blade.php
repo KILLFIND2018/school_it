ec817d5a889f3c33e383af0e14281d1a12aec689
Gulyaev Martinas <killfind@yandex.ru>
2025-10-28 14:58:42 +0300

    Иницианализация и создание каркаса лендинга

---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="about"></div>
+        <div class="why-for-child"></div>
+        <div class="direction"></div>
+        <div class="roadmap_education"></div>
+        <div class="reviews"></div>
+        <div class="certificate_garant"></div>
+        <div class="feedback"></div>
+        <div class="footer"></div>
+    </div>
+</body>
+</html>
